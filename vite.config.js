382145405e94f0fc269880dda35ce52e8823536a
Jesse Kimotho <jesse@drugindex.co.ke>
2026-08-25 22:54:42 +0300

    bathum

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [sveltekit()] });
+export default defineConfig({
+  plugins: [sveltekit()],
+  server: {
+    port: 41729,
+    strictPort: true
+  },
+  preview: {
+    port: 41729,
+    strictPort: true
+  }
+});
